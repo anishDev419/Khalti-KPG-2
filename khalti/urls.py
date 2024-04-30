11fb55fb4427ef0aa4e0c945d80685494ac04436
anishDev419 <manandharanish419@gmail.com>
2024-04-30 12:58:35 +0545

    Handling Return URL

--- khalti/urls.py
+++ khalti/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.Index.as_view(), name="khalti"),
     path('submit/', views.Submit.as_view(), name="submit-khalti"),
     path('success-form-khalti/', views.SuccessForm.as_view(), name="success-form-khalti"),
-    # path('return-khalti/', views.ReturnURL, name="return-khalti"),
+    path('return-khalti/', views.ReturnURL, name="return-khalti"),
     # path('return-khalti/', webhooks.Return_URL, name="return-khalti"),
 ]
--- khalti/views.py
+++ khalti/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView, FormView
 
 from khalti.forms import InitiateForm
@@ -86,50 +87,115 @@ class Submit(FormView):
                     pidx = res_json.get('pidx')
                     payment_url = res_json.get('payment_url')
                     if pidx in payment_url:
+                        # GOOD CASE BEFORE REDIRECTING
                         return HttpResponseRedirect(payment_url)
 
                     else:
                         # INSERT INVOICE ERROR CODE
+                        # HANDLE BAD CASE
                         # HANDLING ERROR MESSAGE
                         print("Error: ", response)
                         return super().form_invalid(form)
 
-
                 elif 'error_key' in res_json:
                     # INSERT INVOICE ERROR CODE
+                    # HANDLE BAD CASE
                     # HANDLING ERROR MESSAGE
                     print("Error: ", response)
                     return super().form_invalid(form)
 
                 else:
                     # INSERT INVOICE ERROR CODE
+                    # HANDLE BAD CASE
                     print("Error: ", response)
                     return super().form_invalid(form)
             else:
                 # INSERT INVOICE ERROR CODE
+                # HANDLE BAD CASE
                 return super().form_invalid(form)
-
-
 
         except Exception as e:
             # INSERT INVOICE ERROR CODE
+            # HANDLE BAD CASE
             return super().form_invalid(form)
 
     def form_invalid(self, form):
+        # HANDLE BAD CASE
+        # HANDLING ERROR MESSAGE
         print("Form errors:", form.errors)
         return super().form_invalid(form)
 
 
+@csrf_exempt
+def ReturnURL(request):
+    if request.method == 'GET':
+        pidx = request.GET.get('pidx')
+        transaction_id = request.GET.get('transaction_id')
+        status = request.GET.get('status')
+
+        if status and status == "Completed" and pidx and transaction_id:
+            # DATA HANDLING VERIFICATION OF TRANSACTION
+
+            # GENERATING URL
+            url = config('khalti_url') + "epayment/lookup/"
+            print("url: ", url)
+
+            # GENERATE SECRET KEY FORMAT
+            auth_key = "Key " + config('khalti_key')
+            payload = {
+                "pidx": pidx,
+            }
+            headers = {
+                'Authorization': auth_key,
+                'Content-Type': 'application/json',
+            }
+
+            response = requests.post(url, headers=headers, json=payload)
+            res_json = response.json()
+
+            if response.status_code == 200:
+
+                if 'pidx' in res_json and 'status' in res_json:
+                    pidx = res_json.get('pidx')
+                    status = res_json.get('status')
+                    if pidx and status and status == "Completed":
+                        # HANDLE GOOD CASE
+                        print("res_json: ", res_json)
+                        return render(request, 'payment/khalti/success.html')
+
+                    else:
+                        # INSERT INVOICE ERROR CODE
+                        # HANDLE BAD CASE
+                        # HANDLING ERROR MESSAGE
+                        print("Error: ", response)
+                        return render(request, 'payment/khalti/failed.html')
+
+                elif 'error_key' in res_json:
+                    # INSERT INVOICE ERROR CODE
+                    # HANDLE BAD CASE
+                    # HANDLING ERROR MESSAGE
+                    print("Error: ", response)
+                    return render(request, 'payment/khalti/failed.html')
+
+                else:
+                    # INSERT INVOICE ERROR CODE
+                    # HANDLE BAD CASE
+                    print("Error: ", response)
+                    return render(request, 'payment/khalti/failed.html')
+            else:
+
+                # INSERT INVOICE ERROR CODE
+                # HANDLE BAD CASE
+                return render(request, 'payment/khalti/failed.html')
+
+        else:
+
+            # INSERT INVOICE ERROR CODE
+            # HANDLE BAD CASE
+            return render(request, 'payment/khalti/failed.html')
+
+
 class SuccessForm(TemplateView):
-    template_name = 'payment/khalti/success.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['message'] = 'Hello World!'
-        return context
-
-
-class ReturnUrl(TemplateView):
     template_name = 'payment/khalti/success.html'
 
     def get_context_data(self, *args, **kwargs):
